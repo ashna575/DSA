b95d54dfd64d1dd10330771e5f2da8518de92c04
ashna23 <ashnaraikwar9@gmail.com>
2025-08-05 23:56:42 +0530

    Assi5Question1

--- Assisment5/Ques1.py
+++ Assisment5/Ques1.py
@@ -1,26 +1,22 @@
-def subarraySum(nums, k):
-    start=num[0]
-   
+def countSubarrays(nums, k):
     count = 0
+    n = len(nums)
 
-    for i in nums:
-        if num[i]+start==k:
-         count+=1
+    for start in range(n):
+        total = 0
+        for end in range(start, n):
+            total += nums[end]
+            if total == k:
+                count += 1
 
     return count
 
-
-num = [1, 2, 3]
+nums = [1, 2, 3]
 k = 3
-print(subarraySum(num, k)) 
+print(countSubarrays(nums, k))  
+
 
 
         
-#  for num in nums:
-#         currentSum += num
-#         if currentSum - k in SumCount:
-#             count += SumCount[currentSum - k]
-#         SumCount[currentSum] = SumCount.get(currentSum, 0) + 1
 
-#     return count
     
