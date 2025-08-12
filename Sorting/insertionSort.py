f1e5912b33896c0fe6c98bd7e10f8f7416c54a30
ashna23 <ashnaraikwar9@gmail.com>
2025-08-12 13:58:12 +0530

    sorting

---
+++ Sorting/insertionSort.py
@@ -0,0 +1,15 @@
+num = [62,44,3,45,63,86,9]
+
+for i in range(1,len(num)):
+    key=num[i]
+    j=i-1
+    while j>=0 and num[j]>key:
+        num[j+1]=num[j]
+        j-=1
+        num[j+1]=key
+        
+print(num)        
+
+
+
+       
