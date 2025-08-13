b67e8ed68623820189365f07d1833b908a5712b8
ashna23 <ashnaraikwar9@gmail.com>
2025-08-13 23:27:24 +0530

    updation

---
+++ Assisment5/palindrom.py
@@ -0,0 +1,16 @@
+word="bbabcbcab"
+n=len(word)
+palindrom=[]
+for i in word(n//2):
+    left = word[i]            
+    right = word[n - 1 - i] 
+    
+         
+  
+
+print(palindrom)        
+
+
+
+
+
--- Assisment5/question4.py
+++ Assisment5/question4.py
@@ -1,1 +1,19 @@
+anagram=["eat","ate","tan","tea","nat","bat"]
+sort={}
+for i in anagram :
 
+    key = ''.join(sorted(i))
+       
+    if key not in sort :
+        sort[key] = []
+    sort[key].append(i)
+
+
+print(sort)
+new_array = list(sort.values())
+print(new_array)
+
+
+  
+ 
+    
