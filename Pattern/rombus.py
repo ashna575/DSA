93389a67110543aef815a059bfd77f12859ad007
ashna23 <ashnaraikwar9@gmail.com>
2025-08-14 23:40:27 +0530

    paternrombus

---
+++ Pattern/rombus.py
@@ -0,0 +1,13 @@
+n = 5  
+
+for i in range(1, n+1):
+    
+    print(" " * (n - i), end="")
+    
+   
+    for j in range(1, n+1):
+        if i == 1 or i == n or j == 1 or j == n:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()  
