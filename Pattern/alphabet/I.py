ebb9626af813ebda01c91dbc7726ad3554fe8e83
ashna23 <ashnaraikwar9@gmail.com>
2025-08-18 22:57:14 +0530

    mop

--- Pattern/alphabet/I.py
+++ Pattern/alphabet/I.py
@@ -1,6 +1,6 @@
 for row in range(7):
     for col in range(5):
-        if (row==0 or row == 6 )or (col==2) :
+        if  (row==0 or row == 6 )or (col==2):
           print("*",end="")
         else:
             print(end=" ")    
