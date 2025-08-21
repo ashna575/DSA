7610ab87069795b8f3286a55d6d7aa10fcd939c0
ashna23 <ashnaraikwar9@gmail.com>
2025-08-21 23:35:28 +0530

    peakval

---
+++ Assisment1/peak.py
@@ -0,0 +1,12 @@
+def findPeak(nums):
+    left, right = 0, len(nums)-1
+    while left < right:
+        mid = (left + right)//2
+        if nums[mid] < nums[mid+1]:
+            left = mid+1
+        else:
+            right = mid
+    return left
+
+print(findPeak([1,2,3,1]))  
+print(findPeak([1,2,1,3,5,6,4]))  
