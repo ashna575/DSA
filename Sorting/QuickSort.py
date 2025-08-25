cbbe93906f86db11442d3809c8d1f9d3a29de869
ashna23 <ashnaraikwar9@gmail.com>
2025-08-25 21:00:02 +0530

    quicksort

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -16,8 +16,35 @@ def Quicksort(arry):
     return Quicksort(left)+mid+Quicksort(righ)
 
 
-
-
-array=[43,542,52,6,42,3,5,6]        
+array=[43,542,52,6,42,3,5,]        
 print(Quicksort(array))        
             
+            
+            
+def partition(arr, low, high):
+    pivot = arr[high]   
+    i = low - 1       
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]  
+
+    
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quicksort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quicksort(arr, low, pi - 1)   
+        quicksort(arr, pi + 1, high)  
+
+
+
+arr = [10, 7, 8, 9, 1, 5]
+
+quicksort(arr, 0, len(arr) - 1)
+print( arr)
