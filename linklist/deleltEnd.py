e6ca7b62f665c7cc1763c99002bb35efa3cb3cd2
ashna23 <ashnaraikwar9@gmail.com>
2025-09-08 23:22:56 +0530

    delend

---
+++ linklist/deleltEnd.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+
+node1=Node(10)
+node2=Node(33)
+node3=Node(34)
+node4=Node(89)
+
+node1.next=node2
+node2.next=node3
+node3.next=node4
+
+head=node1
+
+current=head
+while current.next.next is not None:
+    current=current.next
+current.next=None  
+
+current=head  
+while current is not None:
+    print(current.data,end="->")    
+    current=current.next
+    
