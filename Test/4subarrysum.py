20e3bf7a23c8cb90aea3e8c18cc1572f1c616032
ashna23 <ashnaraikwar9@gmail.com>
2025-09-10 23:37:15 +0530

    subArrayWithSum

---
+++ Test/4subarrysum.py
@@ -0,0 +1,16 @@
+def subarray_with_sum(arr, target):
+    n = len(arr)
+    for i in range(n):
+        s = 0
+        for j in range(i, n):
+            s += arr[j]
+            if s == target:
+                return arr[i:j+1]   
+    return None
+
+
+
+arr = [1, 2, 3, 7, 5]
+target = 12
+result = subarray_with_sum(arr, target)
+print("Subarray:", result)
