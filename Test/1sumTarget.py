8182feca8f5379376bf640d3301214b4d3fb56ee
ashna23 <ashnaraikwar9@gmail.com>
2025-09-10 23:40:20 +0530

    subArrayWithSum

--- Test/1sumTarget.py
+++ Test/1sumTarget.py
@@ -1,6 +1,21 @@
-arry=[1,2,3,14,24,10]
-count=0
-for i in range(0,len(arry)):
-    sum=0
-    for j in range(i,len(arry)):
-        sum+=sum
+def subarray_with_sum(arr, target):
+    n = len(arr)
+    for i in range(n):
+        s = 0
+        for j in range(i, n):
+            s += arr[j]
+            if s == target:
+                return arr[i:j+1], (j - i + 1)   
+    return None, 0
+
+
+
+arr = [1, 2, 3, 7, 5]
+target = 12
+subarr, count = subarray_with_sum(arr, target)
+
+if subarr:
+    print("Subarray:", subarr)
+    print("Count of elements:", count)
+else:
+    print("No subarray found")
