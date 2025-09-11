bff1df1080f115381befe61e637cedddbacffae2
ashna23 <ashnaraikwar9@gmail.com>
2025-09-11 23:25:52 +0530

    push.py

--- Stack/push.py
+++ Stack/push.py
@@ -1,24 +1,18 @@
-stack = []   # empty stack
+stack = []   
 
-# Push operation
 stack.append(10)
 stack.append(20)
 stack.append(30)
 
 print("Stack after push:", stack)
 
-
-
-
-
-
 class Stack:
     def __init__(self, capacity):
-        self.capacity = capacity      # max size of stack
+        self.capacity = capacity      
         self.stack = [None] * capacity
-        self.top = -1                 # empty stack
+        self.top = -1                 
 
-    # Push operation
+    
     def push(self, value):
         if self.top == self.capacity - 1:
             print("Stack is FULL! Cannot push", value)
@@ -27,7 +21,7 @@ class Stack:
             self.stack[self.top] = value
             print(value, "pushed into stack")
 
-    # Display stack
+   
     def display(self):
         if self.top == -1:
             print("Stack is EMPTY!")
@@ -35,10 +29,10 @@ class Stack:
             print("Stack elements:", self.stack[:self.top+1])
 
 
-# Example
+
 s = Stack(3)
 s.push(10)
 s.push(20)
 s.push(30)
-s.push(40)   # should say full
+s.push(40)   
 s.display()
