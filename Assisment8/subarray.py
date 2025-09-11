3e40de57d5be1e4f07444a0a053936f5c73bd5d4
ashna23 <ashnaraikwar9@gmail.com>
2025-09-11 23:31:38 +0530

    subaaray

--- Assisment8/subarray.py
+++ Assisment8/subarray.py
@@ -1,2 +1,18 @@
+arr = [1, 2, 0, 3]
+n = len(arr)
+
+total = sum(arr)
+left_sum = 0
+
+equindex = -1
+
+for i in range(n):
+  
+    if left_sum == (total - left_sum - arr[i]):
+        equindex = i
+        break
+    left_sum += arr[i]
+
+print("Equilibrium Index:", equindex)
 
         
