72b9d3f634272d6509c4ddfc648f60e6e773859a
ashna23 <ashnaraikwar9@gmail.com>
2025-09-11 23:27:34 +0530

    replace

---
+++ Stack/replace#.py
@@ -0,0 +1,5 @@
+# given an input stream s cointaining only lower case alpha while reading char from the stream you 
+# have to tell which char has appear only once in the stream upto that point if these are many char 
+# that have appear onky once you have to tell which one of them was the first one to appear if there is
+#  no such char then append * to the ans
+
