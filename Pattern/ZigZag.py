9c9b66017e534daca8a998eb0ae4454deef68c1c
ashna23 <ashnaraikwar9@gmail.com>
2025-09-15 23:52:24 +0530

    ZigZag

---
+++ Pattern/ZigZag.py
@@ -0,0 +1,12 @@
+# Zig-Zag Number Wave Pattern
+
+n = int(input("Enter number of rows: "))
+m = int(input("Enter number of columns: "))
+
+for i in range(n):
+    for j in range(m):
+        if i % 2 == 0:
+            print(i*m + j + 1, end="\t")
+        else:
+            print(i*m + (m-j), end="\t")
+    print()
