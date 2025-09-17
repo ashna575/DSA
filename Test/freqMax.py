fbba401abb6b508cc228f202189cd7e47270e482
ashna23 <ashnaraikwar9@gmail.com>
2025-09-17 23:24:46 +0530

    maxfreq

---
+++ Test/freqMax.py
@@ -0,0 +1,20 @@
+def frequency(arr):
+    freq = {}
+    for num in arr:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    return freq
+
+
+array = [2, 4, 2, 1, 45, 4, 2, 2]
+
+s = frequency(array)
+print("Frequencies:", s)
+
+for i in s:
+    if s[i] > len(array) / 2: 
+        print(i)
+
+# print("Frequency dictionary:", frequency(array))
