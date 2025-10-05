bd922a820fbe215e3447ae4ccabddbb21be6c07f
ashna23 <ashnaraikwar9@gmail.com>
2025-10-05 23:54:04 +0530

    5oct

--- Test/22mergesub.py
+++ Test/22mergesub.py
@@ -7,15 +7,7 @@ print(sorted(s))
 n = len(arr1)
 m = len(arry2)
 
-# for i in range(n-1, -1, -1):
-     
-#  for j in range(m-1, -1, -1):
-#         if i > j:
-#             s.append(arr1[i])
-#         else:
-#             s.append(arr2[j])
 
-# print(s)
 
 def mergeArrays(arr1, arr2, m, n):
    
