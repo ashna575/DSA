d2a9260ed3d0fc8d441529baa3bea65d5a6c5c5c
ashna23 <ashnaraikwar9@gmail.com>
2025-10-06 21:02:50 +0530

    6oct

---
+++ Test/printNoName.py
@@ -0,0 +1,15 @@
+num = input("Enter a number: ")
+name = {
+    '0': 'Zero',
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine'
+}
+for i in num:
+    print(name[i], end=' ')
