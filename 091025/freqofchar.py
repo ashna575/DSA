65d5913c24c317fee53d8dbc794f9b9b4063c5fd
ashna23 <ashnaraikwar9@gmail.com>
2025-10-09 23:43:03 +0530

    9oct

---
+++ 091025/freqofchar.py
@@ -0,0 +1,12 @@
+
+string = input("Enter a string: ")
+
+freq = {}
+
+for char in string:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+for key, value in freq.items():
+    print(f"{key}: {value}")
